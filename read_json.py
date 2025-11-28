0f4d6c9641df88f259433e62617a31f62dd95e93
Francisco Caldeira <kikocaldeira2007@gmail.com>
2025-11-28 15:05:42 +0000

    exercicio 1

--- read_json.py
+++ read_json.py
@@ -2,18 +2,18 @@ import json
 import os.path
 path="data.json"
 if os.path.exists(path)==False:
-    print("ERRO: Ficheiro não existe!")
+    print("ERRO: Ficheiro não existe")
 elif ".json" not in path:
-    print("ERRO: Ficheiro não é JSON!")
+    print("ERRO: Ficheiro não é JSON")
 else:
     file = open(path, "rt",encoding='utf-8')
     json_data = file.read()
     if json_data=='' or json_data=='{}':
-        print("ERRO: O ficheiro está vazio!!")
+        print("ERRO: O ficheiro está vazio")
     else:
         data = json.loads(json_data)
         if "idade" not in data or "nome" not in data or "localização" not in data:
-            print("ERRO: Campos obrigatórios em falta!!!")
+            print("ERRO: Campos obrigatórios em falta")
         else:
             print(data)
             file.close()
